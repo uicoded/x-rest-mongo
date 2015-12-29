64ad1142d301c8a78baa8c4f3002651fdbae9960
uicoded <uicoded@gmail.com>
2015-12-29 16:57:56 -0500

    code clean - taking routes out

--- app.js
+++ app.js
@@ -9,60 +9,16 @@ var port = process.env.PORT || 3000;
 //app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-/* Models */
+// Book model
 var Book = require("./models/bookModel");
-
-var router = express.Router();
-
-// Plural "Books" seem to be more common naming
-router.route("/Books")
-    .post(function (req, res) {
-        var book = new Book(req.body);
-        //  console.log(book);
-
-        // save the book in mongodb
-        book.save();
-        res.status(201).send(book);
-    })
-    .get(function (req, res) {
-    //var responseJson = {
-    //    hello: "Book A"
-    //};
-    //res.json(responseJson);
-
-    //not good idea to directly use user input
-    //var query = req.query;
-    var query = {};
-    if (req.query.genre) {
-        //in this way only genre query gets through
-        query.genre = req.query.genre;
-    }
-
-    Book.find(query, function (err, books) {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.json(books);
-        }
-    });
-});
-
-router.route("/Books/:bookId").get(function (req, res) {
-    Book.findById(req.params.bookId, function (err, book) {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.json(book);
-        }
-    })
-});
+var router = require("./routes/book.routes.js")(Book);
 
 
 app.use("/api", router);
 
 
 app.get("/", function (req, res) {
-    res.send('Welcome to API')
+    res.send('Welcome to Book API example. Go to <a href="api/Books">books</a>');
 });
 
 app.listen(port, function () {
---
+++ routes/book.routes.js
@@ -0,0 +1,62 @@
+var express = require("express");
+
+/**
+ * Resource
+ *
+ * Uses Express router to make REST API for common resource
+ * For now it uses Book but can be genelized.
+ *
+ * @param Book
+ * @returns {Object} router
+ */
+var routes = function resource (Book) {
+
+    // http://expressjs.com/en/4x/api.html#router
+    var router = express.Router();
+
+    //console.log(Book.toString());
+
+    // Plural "Books" seem to be more common naming
+    // TODO: find out if it is instance or can be extended (getting singleton)
+    router.route("/Books")
+        .post(function (req, res) {
+            var book = new Book(req.body);
+            // console.log(book);
+
+            // save the book in mongodb
+            book.save();
+            res.status(201).send(book);
+        })
+        .get(function (req, res) {
+
+            // not good idea to directly use user input
+            // var query = req.query;
+            var query = {};
+            if (req.query.genre) {
+                //in this way only genre query gets through
+                query.genre = req.query.genre;
+            }
+
+            Book.find(query, function (err, books) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(books);
+                }
+            });
+        });
+
+    router.route("/Books/:bookId").get(function (req, res) {
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(book);
+            }
+        })
+    });
+
+    return router;
+};
+
+module.exports = routes;
